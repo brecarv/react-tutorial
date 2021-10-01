49d3927b27ad231a89dca599c8b4a9a2694e6ceb
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2021-10-01 15:15:26 -0300

    Square: render Props

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
